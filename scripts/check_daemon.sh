e2fb2bedfe472e44ece792f73b598a935775da43
arturgontijo <arturgontijo@gmail.com>
2020-03-11 11:32:33 -0300

    [org] Checking and Updating Daemon if necessary.

--- scripts/check_daemon.sh
+++ scripts/check_daemon.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-RET_VAL=0
-
 export SNETD_VERSION=$1
 export SNETD_CURR_VERSION=""
 if [ "${SNETD_VERSION}" != "" ]; then
@@ -9,7 +7,6 @@ if [ "${SNETD_VERSION}" != "" ]; then
 			     docker exec "${PROD_TAG}""${DOCKER_CONTAINER}" snetd version | grep -oP 'version tag: \K(.*)' \
 			     || echo "")
 	if [ "${SNETD_CURR_VERSION}" = "" ] || [ "${SNETD_VERSION}" != "${SNETD_CURR_VERSION}" ]; then
-		RET_VAL=1
 		ssh -o "StrictHostKeyChecking no" "${SSH_USER}"@"${SSH_HOST}" <<- EOF
 			cd /tmp || true
 			wget https://github.com/singnet/snet-daemon/releases/download/"${SNETD_VERSION}"/snet-daemon-"${SNETD_VERSION}"-linux-amd64.tar.gz
@@ -20,4 +17,4 @@ if [ "${SNETD_VERSION}" != "" ]; then
 	fi
 fi
 
-echo ${RET_VAL}
+echo 0
