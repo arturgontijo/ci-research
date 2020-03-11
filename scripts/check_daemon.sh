4e350fce25009501ca419488a4c8f8d28d5dfaba
arturgontijo <arturgontijo@gmail.com>
2020-03-11 11:48:10 -0300

    [org] Checking and Updating Daemon if necessary.

--- scripts/check_daemon.sh
+++ scripts/check_daemon.sh
@@ -4,15 +4,15 @@ export SNETD_VERSION=$1
 export SNETD_CURR_VERSION=""
 if [ "${SNETD_VERSION}" != "" ]; then
 	SNETD_CURR_VERSION=$(ssh -o "StrictHostKeyChecking no" "${SSH_USER}"@"${SSH_HOST}" \
-			     docker exec "${PROD_TAG}""${DOCKER_CONTAINER}" snetd version | grep -oP 'version tag: \K(.*)' \
-			     || echo "")
+						docker exec "${PROD_TAG}""${DOCKER_CONTAINER}" snetd version | grep -oP 'version tag: \K(.*)' \
+						|| echo "")
 	if [ "${SNETD_CURR_VERSION}" = "" ] || [ "${SNETD_VERSION}" != "${SNETD_CURR_VERSION}" ]; then
 		ssh -o "StrictHostKeyChecking no" "${SSH_USER}"@"${SSH_HOST}" <<- EOF
-		cd /tmp || true
-		wget https://github.com/singnet/snet-daemon/releases/download/"${SNETD_VERSION}"/snet-daemon-"${SNETD_VERSION}"-linux-amd64.tar.gz
-		tar -xvf snet-daemon-"${SNETD_VERSION}"-linux-amd64.tar.gz
-		docker cp snet-daemon-"${SNETD_VERSION}"-linux-amd64/snetd "${PROD_TAG}""${DOCKER_CONTAINER}":/usr/bin || echo true
-		rm -rf snet-daemon-*
+			cd /tmp || true
+			wget https://github.com/singnet/snet-daemon/releases/download/"${SNETD_VERSION}"/snet-daemon-"${SNETD_VERSION}"-linux-amd64.tar.gz
+			tar -xvf snet-daemon-"${SNETD_VERSION}"-linux-amd64.tar.gz
+			docker cp snet-daemon-"${SNETD_VERSION}"-linux-amd64/snetd "${PROD_TAG}""${DOCKER_CONTAINER}":/usr/bin || echo true
+			rm -rf snet-daemon-*
 		EOF
 	fi
 fi
